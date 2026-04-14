32de2a30ed91eecbcbf628708b3fa3a204a50fd6
Felipe Chaparro <luis.chaparroh@usantoto.edu.co>
2026-04-14 16:09:03 -0500

    202604141603 devlfc subiendo conexion a servicios con componentes y arquitectura organizada x2

--- src/app/features/perfil/perfil.routes.ts
+++ src/app/features/perfil/perfil.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const perfilRoutes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/perfil/perfil.page').then(m => m.PerfilPageComponent),
+    loadComponent: () => import('./perfil.page').then(m => m.PerfilPageComponent),
   },
 ];
